340b4a49c4d85c3c4b695ac6de4b9724b6b0b8c0
javakidx <javakidx@gmail.com>
2015-04-08 01:08:18 +0800

    Initail commit

--- src/idv/jk/util/lang/StringMain.java
+++ src/idv/jk/util/lang/StringMain.java
@@ -2,6 +2,8 @@ package idv.jk.util.lang;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.Arrays;
+
 /**
  * Created by bioyang on 15/4/7.
  */
@@ -9,7 +11,13 @@ public class StringMain
 {
     public static void main(String[] argvs)
     {
-        String input = "";
+        String input = "1,2,,4,5,";
+        String[] peices = input.split(",");
+        System.out.println("字串陣列的長度為: " + peices.length);
+        System.out.print("字串陣列內的元素為: ");
+        System.out.print(Arrays.asList(peices));
+        System.out.println();
+        split();
     }
 
     public boolean checkInput(String input)
@@ -30,5 +38,14 @@ public class StringMain
         //StringUtils.isEmpty("")   回傳true
         //StringUtils.isEmpty(" ")  回傳false
         return true;
+    }
+
+    public static void split()
+    {
+        String input = "1,2,,4,5,";
+        String[] peices = StringUtils.splitByWholeSeparatorPreserveAllTokens(input, ",");
+        System.out.println("字串陣列的長度為: " + peices.length);
+        System.out.print("字串陣列內的元素為: ");
+        System.out.print(Arrays.asList(peices));
     }
 }
