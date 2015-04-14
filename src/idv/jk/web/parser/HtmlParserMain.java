156e3d0c0dab6791f979acf2caed53376f94c3ea
javakidx <javakidx@gmail.com>
2015-04-14 14:40:31 +0800

    Initail commit

---
+++ src/idv/jk/web/parser/HtmlParserMain.java
@@ -0,0 +1,34 @@
+package idv.jk.web.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Created by 安軻 on 2015/4/14.
+ */
+public class HtmlParserMain
+{
+    public static void main(String[] argv)
+    {
+        String url = "https://news.google.com.tw/news";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            System.out.println(doc.title());
+            Elements h1s = doc.select("span.titletext");
+
+            Element thisOne = null;
+            for(Iterator<Element> it = h1s.iterator(); it.hasNext();)
+            {
+                thisOne = it.next();
+                System.out.println(thisOne.html());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
