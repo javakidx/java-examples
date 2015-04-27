30cc3cade6186598bd4aaa8c7adf8f733c6c5e90
javakidx <javakidx@gmail.com>
2015-04-27 18:14:49 +0800

    Initail commit

---
+++ src/idv/jk/util/lang/CharMain.java
@@ -0,0 +1,13 @@
+package idv.jk.util.lang;
+
+/**
+ * Created by 安軻 on 2015/4/27.
+ */
+public class CharMain
+{
+    public static void main(String[] argv)
+    {
+        char c32 = 33;
+        System.out.println(c32);
+    }
+}
