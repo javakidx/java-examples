1342817a11a084435b19cad04d1e1761b22d7742
javakidx <javakidx@gmail.com>
2015-10-14 01:57:07 +0800

    Initail commit

---
+++ src/idv/jk/study/designpattern/state/MoodState.java
@@ -0,0 +1,9 @@
+package idv.jk.study.designpattern.state;
+
+/**
+ * Created by bioyang on 2015/10/14.
+ */
+public abstract class MoodState
+{
+    public abstract String getMood(Developer developer);
+}
