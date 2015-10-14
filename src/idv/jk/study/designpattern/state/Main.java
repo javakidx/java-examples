c61095559bfeb160aec3e1f5baa61d33b50d2011
YoungUncle <bioankeyang@gmail.com>
2015-10-14 09:34:29 +0800

    comment

--- src/idv/jk/study/designpattern/state/Main.java
+++ src/idv/jk/study/designpattern/state/Main.java
@@ -7,18 +7,24 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //新的一天開始的大熊
         Developer giantBear = new Developer("大熊");
+        //來看他的心情如何
         giantBear.sayWhatAreThinkingNow();
 
+        //到了九點的大熊
         giantBear.setHour(9);
         giantBear.sayWhatAreThinkingNow();
 
+        //到了中午吃飯時間
         giantBear.setHour(12);
         giantBear.sayWhatAreThinkingNow();
 
+        //午休結束，開始上班
         giantBear.setHour(13);
         giantBear.sayWhatAreThinkingNow();
 
+        //下班了
         giantBear.setHour(18);
         giantBear.sayWhatAreThinkingNow();
     }
