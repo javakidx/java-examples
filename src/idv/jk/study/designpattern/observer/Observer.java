7eeb6178d0e7abdfb82c1b868f890f74d80179b7
YoungUncle <bioankeyang@gmail.com>
2015-10-15 13:28:43 +0800

    Observer pattern

---
+++ src/idv/jk/study/designpattern/observer/Observer.java
@@ -0,0 +1,14 @@
+package idv.jk.study.designpattern.observer;
+
+/**
+ * Created by bioyang on 2015/10/15.
+ */
+public abstract class Observer
+{
+    protected String mName;
+    protected Subject mSubject;
+
+    public abstract void update();
+
+
+}
---
+++ src/idv/jk/study/designpattern/observer/SheepDog.java
@@ -0,0 +1,8 @@
+package idv.jk.study.designpattern.observer;
+
+/**
+ * Created by bioyang on 2015/10/15.
+ */
+public class SheepDog extends Subject
+{
+}
