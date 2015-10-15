f02a3eac62024f90777b1a3c32e831f6c1a26259
YoungUncle <bioankeyang@gmail.com>
2015-10-15 21:13:08 +0800

    Knight on chessboard

---
+++ src/idv/jk/fun/KnightChess.java
@@ -0,0 +1,24 @@
+package idv.jk.fun;
+
+/**
+ * Created by bioyang on 2015/10/15.
+ */
+public class KnightChess
+{
+    public static void main(String[] args)
+    {
+        int[][] chess = new int[8][8];
+
+        int i = 4;
+        int j = 5;
+        for(int k = 1; k <= 64; k++ )
+        {
+
+        }
+    }
+
+    public void knightStep(int i, int j, int k, int[][] chessBoard)
+    {
+
+    }
+}
