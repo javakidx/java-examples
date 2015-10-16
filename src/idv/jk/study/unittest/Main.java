a439a0b437610a2ead55a1fb7cfdad35ab1d6651
YoungUncle <bioankeyang@gmail.com>
2015-10-16 18:24:02 +0800

    Initail commit

---
+++ src/idv/jk/study/unittest/Main.java
@@ -0,0 +1,17 @@
+package idv.jk.study.unittest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by bioyang on 2015/10/16.
+ */
+public class Main
+{
+    @Test
+    public void testPrint()
+    {
+        boolean i = true;
+        assertTrue(i);
+    }
+}
